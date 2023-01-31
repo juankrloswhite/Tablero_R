b7629a157c05a4ea03e4b8b6b3ba683e3e9a32ee
Juan Carlos Restrepo Blanco <carlos.restrepo@avianca.com>
2023-01-31 11:41:49 -0500

    Instalar Paqueteria

--- TaBleroJuanRestrepo.R
+++ TaBleroJuanRestrepo.R
@@ -1,1 +1,12 @@
-## Tablero Juan Restrepo
+#### Tablero Juan Restrepo ####
+
+##Instalacion de paqueteria 
+install.packages("devtools")  # instala paquete devtools
+devtools::install_github("dgonxalex80/paqueteMET") # descarga de paqueteMET
+library(paqueteMET)     # activar paqueteMET
+
+data("CarreraLuz22")    # cargar la dataset
+
+install.packages("remotes")  
+remotes::install_github("rstudio/bslib")  # descarga paquete que contiene formato 
+
